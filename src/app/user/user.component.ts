9115a6c01a7c298e14d63ea0cf29bd7c8d7c83d1
Jayeson Kiyabu and Philip Putnam <jayeson@>
2017-04-10 10:46:32 -0700

    successfull display firebase data on user page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { CallService } from '../call.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [CallService]
 })
 export class UserComponent implements OnInit {
+  calls: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private callService: CallService) { }
 
   ngOnInit() {
+    this.calls = this.callService.getCalls();
   }
 
 }
