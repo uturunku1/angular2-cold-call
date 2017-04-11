0bfef4abe484d4cc65addee77d2d0c18ae700c61
Jayeson Kiyabu and Stella Huayhuaca <jayeson@>
2017-04-11 08:45:38 -0700

    update user component

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -14,7 +14,7 @@ export class IndexComponent implements OnInit {
 
   points: number = 0;
 
-  constructor(public callService: CallService) { }
+  constructor(public callService: CallService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -26,6 +26,7 @@ export class IndexComponent implements OnInit {
   submitCall(clientName: string, companyName: string, email: string, location: string, date: string, phoneNumber: string, description: string){
     let newCall = new Call(clientName, companyName, email, location, date, phoneNumber, description, this.points);
     this.callService.addCall(newCall);
+    this.router.navigate(['user']);
   }
 
 }
